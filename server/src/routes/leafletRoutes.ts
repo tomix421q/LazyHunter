3ffd6a4a55a7b9bcd0bf59c5faa992652a19e2c6
tomix421q <zilka.tomas421@gmail.com>
2026-05-07 00:57:27 +0200

    .

--- server/src/routes/leafletRoutes.ts
+++ server/src/routes/leafletRoutes.ts
@@ -3,4 +3,6 @@ import { leafletsController } from '../controllers/leafletsController'
 
 const leafletsService_Route = new Hono()
 
+leafletsService_Route.post('/trigger-hunt', leafletsController.get_leaflet)
+
 export default leafletsService_Route
